54a98593bd5f06106aa5d5525d55cd6ab17c9ead
gdalal <IOs09000>
2020-07-26 12:42:18 +0300

    in PI set default for double_q=False, and made policy partial with dueling=False. Next, add single_output argument in FeedForward policy to change the fully-connected output

--- run_experiment.py
+++ run_experiment.py
@@ -3,11 +3,12 @@ import wandb
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines.deepq.policies import MlpPolicy, LnCnnPolicy, CnnPolicy
 from stable_baselines import DQN
+from stable_baselines.deepq.policy_iteration import PI
 from stable_baselines.common.atari_wrappers import make_atari
 from stable_baselines.common.callbacks import CheckpointCallback
 ENV_NAME = 'MsPacman-v0'
 AGENT_NAME = 'agent_' + ENV_NAME
-TOTAL_TIMESTEPS = int(50e6)
+TOTAL_TIMESTEPS = int(2e6)
 
 
 wandb.init(project="stable_baselines-dqn")
@@ -19,7 +20,8 @@ agent_full_name = wandb.run.id + '_' + AGENT_NAME
 checkpoint_callback = CheckpointCallback(save_freq=30*1800, save_path='./logs/',
                                          name_prefix=agent_full_name)
 
-model = DQN(LnCnnPolicy, env, verbose=1, train_freq=4, exploration_fraction=0.01, learning_rate=0.0001)
+# model = DQN(LnCnnPolicy, env, verbose=1, train_freq=4, exploration_fraction=0.01, learning_rate=0.0001)
+model = PI(LnCnnPolicy, env, verbose=1, train_freq=4, exploration_fraction=0.01, learning_rate=0.0001)
 model.learn(total_timesteps=TOTAL_TIMESTEPS, callback=checkpoint_callback)
 model.save(agent_full_name)
 
--- stable_baselines/deepq/policy_iteration.py
+++ stable_baselines/deepq/policy_iteration.py
@@ -56,13 +56,14 @@ class PI(OffPolicyRLModel):
         If None, the number of cpu of the current machine will be used.
     """
     def __init__(self, policy, env, gamma=0.99, learning_rate=5e-4, buffer_size=50000, exploration_fraction=0.1,
-                 exploration_final_eps=0.02, exploration_initial_eps=1.0, train_freq=1, batch_size=32, double_q=True,
+                 exploration_final_eps=0.02, exploration_initial_eps=1.0, train_freq=1, batch_size=32, double_q=False,
                  learning_starts=1000, target_network_update_freq=500, prioritized_replay=False,
                  prioritized_replay_alpha=0.6, prioritized_replay_beta0=0.4, prioritized_replay_beta_iters=None,
                  prioritized_replay_eps=1e-6, param_noise=False,
                  n_cpu_tf_sess=None, verbose=0, tensorboard_log=None,
                  _init_setup_model=True, policy_kwargs=None, full_tensorboard_log=False, seed=None):
 
+        policy = partial(policy, dueling=False)
         # TODO: replay_buffer refactoring
         super(PI, self).__init__(policy=policy, env=env, replay_buffer=None, verbose=verbose, policy_base=DQNPolicy,
                                   requires_vec_env=False, policy_kwargs=policy_kwargs, seed=seed, n_cpu_tf_sess=n_cpu_tf_sess)
