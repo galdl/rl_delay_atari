23c24f7cdbcb7175cab0a6c661668abe0d8d89b5
Gal Dalal <gdalal@nvidia.com>
2020-08-03 14:21:09 +0300

    added wandb sweep

--- run_experiment.py
+++ run_experiment.py
@@ -6,8 +6,8 @@ from stable_baselines import DQN
 from stable_baselines.deepq.policy_iteration import PI
 from stable_baselines.common.atari_wrappers import make_atari
 from stable_baselines.common.callbacks import CheckpointCallback
-ENV_NAME = 'MsPacman-v0'
-AGENT_NAME = 'agent_' + ENV_NAME
+# ENV_NAME = 'MsPacman-v0'
+AGENT_NAME = 'agent_'# + ENV_NAME
 TOTAL_TIMESTEPS = int(2e6)
 
 hyperparameter_defaults = dict(
@@ -16,26 +16,31 @@ hyperparameter_defaults = dict(
     learning_rate=0.0001,
     target_network_update_freq=500,
     exploration_final_eps=0.01,
-    seed=1
+    seed=1,
+    env_name='MsPacman-v0',
+    use_pi=True,
 )
 # Pass your defaults to wandb.init
 wandb.init(config=hyperparameter_defaults, project="stable_baselines-dqn")
 config = wandb.config
 
-env = gym.make(ENV_NAME)
+env = gym.make(config.env_name)
 # env = make_atari('BreakoutNoFrameskip-v4')
 agent_full_name = wandb.run.id + '_' + AGENT_NAME
 # Save a checkpoint every 1000 steps
 checkpoint_callback = CheckpointCallback(save_freq=30*1800, save_path='./logs/',
                                          name_prefix=agent_full_name)
 
-# model = DQN(LnCnnPolicy, env, verbose=1, train_freq=4, exploration_fraction=0.01, learning_rate=0.0001)
-model = PI(LnCnnPolicy, env, verbose=1, train_freq=config.train_freq,
-           exploration_initial_eps=config.exploration_initial_eps,
-           exploration_fraction=0.01, learning_rate=config.learning_rate,
-           target_network_update_freq=config.target_network_update_freq,
-           exploration_final_eps=config.exploration_final_eps)
-model.learn(total_timesteps=TOTAL_TIMESTEPS, callback=checkpoint_callback)
+if config.use_pi:
+    model = PI(LnCnnPolicy, env, verbose=1, train_freq=config.train_freq,
+               exploration_initial_eps=config.exploration_initial_eps,
+               exploration_fraction=0.01, learning_rate=config.learning_rate,
+               target_network_update_freq=config.target_network_update_freq,
+               exploration_final_eps=config.exploration_final_eps)
+    model.learn(total_timesteps=TOTAL_TIMESTEPS, callback=checkpoint_callback)
+else:
+    model = DQN(LnCnnPolicy, env, verbose=1, train_freq=4, exploration_fraction=0.01, learning_rate=0.0001)
+
 model.save(agent_full_name)
 
 # del model # remove to demonstrate saving and loading
