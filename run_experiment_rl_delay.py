2d7cd9b4e13d149ed2c34772d794ea4bac405fc0
Gal Dalal <gdalal@nvidia.com>
2021-04-25 21:49:14 +0300

    started cleaning up

--- run_experiment_rl_delay.py
+++ run_experiment_rl_delay.py
@@ -3,15 +3,12 @@ import wandb
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines.deepq.policies import LnCnnPolicy
 from stable_baselines.common.policies import CnnLnLstmPolicy
-from stable_baselines import logger, A2C
-from stable_baselines import DQN
-from stable_baselines.deepq.policy_iteration import PI
+from stable_baselines import A2C
 from stable_baselines.deepq.delayed_dqn import DelayedDQN
-from stable_baselines.common.atari_wrappers import make_atari, DelayWrapper, MaxAndSkipEnv, wrap_deepmind
-from stable_baselines.common.callbacks import CheckpointCallback
-from stable_baselines.common.vec_env import VecFrameStack, VecNormalize, SubprocVecEnv
+from stable_baselines.common.atari_wrappers import DelayWrapper, MaxAndSkipEnv, wrap_deepmind
 from functools import partial
 import numpy as np
+
 AVERAGE_OVER_LAST_EP = 0.05
 
 def make_delayed_env(config):
@@ -28,8 +25,6 @@ import platform
 if platform.system() == 'Darwin':
     import os
     os.environ['KMP_DUPLICATE_LIB_OK']='True'
-
-# TOTAL_TIMESTEPS = int(1e6)
 
 hyperparameter_defaults = dict(
     train_freq=4,
@@ -48,27 +43,19 @@ hyperparameter_defaults = dict(
     load_pretrained_agent=False,
     agent_type='delayed', #'delayed', 'augmented', 'oblivious', 'rnn'
     num_rnn_envs=4,
-    deepmind_wrapper=True,
+    deepmind_wrapper=False,
     total_timesteps=int(1e6)
 )
 # Pass your defaults to wandb.init
 wandb.init(config=hyperparameter_defaults, project="stable_baselines_tf-rl_delay")
 config = wandb.config
 
-
-#TODO: check if using fixed 4-frame skip is better
-# env = make_atari('BreakoutNoFrameskip-v4')
 agent_full_name = wandb.run.id + '_' + AGENT_NAME
 # Save a checkpoint every 1000 steps
 # checkpoint_callback = CheckpointCallback(save_freq=30*1800, save_path='./logs/',
 #                                          name_prefix=agent_full_name)
 checkpoint_callback = None
-# model = DQN(LnCnnPolicy, env, verbose=1, train_freq=config.train_freq, learning_rate=config.learning_rate,
-#                 double_q=True, target_network_update_freq=config.target_network_update_freq,
-#             gamma=config.gamma, prioritized_replay=True, exploration_initial_eps=config.exploration_initial_eps,
-#             exploration_final_eps=config.exploration_final_eps)
 if config.agent_type == 'rnn':
-    # env = SubprocVecEnv([make_delayed_env(config) for i in range(config.num_rnn_envs)])
     env = DummyVecEnv([partial(make_delayed_env, config=config)])
     model = A2C(CnnLnLstmPolicy, env, verbose=1, gamma=config.gamma, tensorboard_log='')
 else:
@@ -96,14 +83,3 @@ avg_over = round(tot_ep_num * AVERAGE_OVER_LAST_EP)
 final_avg_score = np.mean(episode_rewards[-avg_over:])
 wandb.log({'final_score': final_avg_score})
 
-# model.save(agent_full_name)
-
-# del model # remove to demonstrate saving and loading
-#
-# model = DQN.load("deepq_cartpole")
-#
-# obs = env.reset()
-# while True:
-#     action, _states = model.predict(obs)
-#     obs, rewards, dones, info = env.step(action)
-#     env.render()
